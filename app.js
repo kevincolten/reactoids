231998c3ef1666bb10863c3a8d6b4bf6612cb364
Kevin Colten <kevincolten@gmail.com>
2015-07-19 11:21:42 -0500

    Asteroids Moving

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 'use strict';
 
+var AsteroidModel = require('./models/AsteroidModel');
+var AsteroidsComponent = require('./components/AsteroidsComponent');
 var Backbone = require('backbone');
-var PostsController = require('./controllers/PostsController');
+var React = require('react');
+var _ = require('underscore');
 
 var Application = Backbone.Router.extend({
-  controllers: {},
+  initialize: function()
+  {
+    var asteroids = new Backbone.Collection();
 
-  initialize: function() {
-    this.controllers.posts = new PostsController({router: this});
-    Backbone.history.start();
+    _(5).times(function () {
+      asteroids.add(new AsteroidModel());
+    });
+
+    var AsteroidsView = React.createFactory(AsteroidsComponent);
+    React.render(AsteroidsView({ collection: asteroids }), document.getElementById('content'));
+
+    setInterval (function () {
+      asteroids.each(function (asteroid) {
+        asteroid.set({
+          pos_x: asteroid.get('pos_x') + asteroid.get('vel_x'),
+          pos_y: asteroid.get('pos_y') + asteroid.get('vel_y'),
+        });
+        if (asteroid.offScreen()) {
+          asteroid.fixOffScreen();
+        }
+      });
+    }, 10 );
+
   }
 });
 
--- components/AsteroidsComponent.js
+++ components/AsteroidsComponent.js
@@ -1,13 +1,18 @@
 var React = require('react');
 var Backbone = require('backbone');
 require('react.backbone');
+var AsteroidComponent = require('./AsteroidComponent');
 
 module.exports = React.createBackboneClass({
+
   render: function() {
+    var asteroids = this.props.collection.map(function (asteroid) {
+      return <AsteroidComponent model={asteroid} />
+    });
+
     return (
       <div>
-        <h1>{this.props.model.get('description')}</h1>
-        <p dangerouslySetInnerHTML={{__html: this.props.model.get('content')}}></p>
+        {asteroids}
       </div>
     );
   }
