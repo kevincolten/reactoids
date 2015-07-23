8e35cf26f0d4dad59c836705ebc03ccc4f435d8b
Kevin Colten <kevincolten@gmail.com>
2015-07-23 13:28:22 -0500

    uglifying

--- bundle.js
+++ bundle.js
@@ -165,13 +165,14 @@ module.exports = Backbone.Model.extend({
   fixOffScreen: function()
   {
     if (this.get('pos_x') < 0) {
-        this.set('pos_x', this.get('pos_x') + window.innerWidth);
-    } else if(this.get('pos_x') > window.innerWidth) {
-        this.set('pos_x', this.get('pos_x') - window.innerWidth);
-    } else if(this.get('pos_y') < 0) {
-        this.set('pos_y', this.get('pos_y') + window.innerHeight);
-    } else if(this.get('vel_y') > window.innerHeight) {
-        this.set('pos_y', this.get('pos_y') - window.innerHeight);
+      this.set('pos_x', this.get('pos_x') + window.innerWidth);
+    } else if (this.get('pos_x') > window.innerWidth) {
+      this.set('pos_x', this.get('pos_x') - window.innerWidth);
+    }
+    if (this.get('pos_y') < 0) {
+      this.set('pos_y', this.get('pos_y') + window.innerHeight);
+    } else if (this.get('pos_y') > window.innerHeight) {
+      this.set('pos_y', this.get('pos_y') - window.innerHeight);
     }
   }
 });
--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 var assign = require('lodash.assign');
 var reactify = require('reactify');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 // add custom browserify options here
 var customOpts = {
@@ -30,7 +32,7 @@ b.on('log', gutil.log); // output build logs to terminal
 function bundle() {
   return b.bundle()
     .pipe(source('bundle.js')) // simply pipes in a filename
-    .pipe(buffer()) // more info here https://medium.com/@webprolific/getting-gulpy-a2010c13d3d5
+    .pipe(buffer())
 
     // build sourcemaps
     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
@@ -38,3 +40,10 @@ function bundle() {
 
     .pipe(gulp.dest('./')); // destination of bundle.js
 }
+
+gulp.task('uglify', function() {
+  return gulp.src('bundle.js')
+    .pipe(uglify())
+    .pipe(rename('bundle.min.js'))
+    .pipe(gulp.dest('./'));
+});
